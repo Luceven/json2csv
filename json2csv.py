a62298d54bce88756dad8559b66240a77cbebee0
Luceven <yzeng17@hotmail.com>
2020-07-29 00:28:51 -0400

    created user interface, changed functions

--- json2csv.py
+++ json2csv.py
@@ -4,25 +4,38 @@ import json
 import csv
 import glob
 from tkinter import *
+from tkinter.filedialog import askdirectory
+
+def onEnterDir(dropdown, var):
+    path = askdirectory()
+    if not path:
+        return
+    filenames = glob.glob("*.json")
+    dropdown.configure(state='normal')  # Enable drop down
+    menu = dropdown['menu']
+
+    # Clear the menu
+    menu.delete(0, 'end')
+    for name in filenames:
+        # Add menu items
+        menu.add_command(label=name, command=lambda name=name: var.set(name))
+        # OR menu.add_command(label=name, command=partial(var.set, name))
 
 def main():
-	# get all json files
-	filenames = glob.glob("*.json")
-
 	# create user interface using Tkinter
 	app = Tk()
 	app.title("json2csv v1.0")
-	app.geometry('220x150')
+	app.geometry('280x150')
 	# create a Tkinter variable
 	tkvar = StringVar(app)
-	# options copied from glob list
-	choices = filenames
-	# set the default option
-	tkvar.set(choices[0])
 
-	dropdown = OptionMenu(app, tkvar, *choices)
-	Label(app, text="Please choose json file").grid(row=2, column=2)
-	dropdown.grid(row=3, column=2)
+	dropdown = OptionMenu(app, tkvar, "Select JSON...")
+	Label(app, text="Please choose json file").grid(row=1, column=1)
+	dropdown.grid(row=2, column=2)
+	dropdown.configure(state='disabled')
+
+	b = Button(app, text='Change Directory', command=lambda: onEnterDir(dropdown, tkvar))
+	b.grid(row=2, column=1)
 
 	def convert_json(*args):
 		global name
@@ -34,7 +47,7 @@ def main():
 
 		# convert file
 		csv_name = name[:-5] + '.csv'
-		output = open(csv_name, 'w')
+		output = open(csv_name, 'w', encoding='utf-8')
 		csvwriter = csv.writer(output)
 		count = 0
 		for dat in data:
@@ -48,19 +61,13 @@ def main():
 		labelTest.configure(text="{} converted!".format(name))
 
 	b1 = Button(app, text='Convert', command=convert_json)
-	b1.grid(row=3, column=3)
+	b1.grid(row=10, column=1)
 	b2 = Button(app, text='Close', command=app.quit)
-	b2.grid(row=6, column=2)
+	b2.grid(row=10, column=2)
 
 	labelTest = Label(text="", fg='red')
-	labelTest.grid(row=8, column=2)
+	labelTest.grid(row=4, column=1)
 
-	# on change dropdown value
-	#def change_dropdown(*args):
-		#labelTest.configure(text="{} converted!".format(tkvar.get()))
-
-	# link function to change dropdown
-	#tkvar.trace('w', change_dropdown)
 	app.mainloop()
 
 if __name__ == "__main__":
